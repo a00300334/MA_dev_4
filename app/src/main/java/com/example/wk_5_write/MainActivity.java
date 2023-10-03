584b864b62728e36a44fce31eb0cfdb665437c24
a00300334 <a00300334@gmail.com>
2023-10-03 12:50:40 +0100

    working file saving api 26

--- app/src/main/java/com/example/wk_5_write/MainActivity.java
+++ app/src/main/java/com/example/wk_5_write/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.wk_5_write;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.Manifest;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int STORAGE_PERMISSION_CODE = 123;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
             String[] permissions = {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
+
             };
 
             ActivityCompat.requestPermissions(
@@ -38,9 +47,42 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode == STORAGE_PERMISSION_CODE){
             // this is handling the Storage request
+
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // user grantet permission for WRITE_EXTERNAL_STORAGE
+                recreate();
+            }else{
+                //user denied the permission
+                Toast.makeText(this, "This app require Storage, please allow it.", Toast.LENGTH_LONG).show();
+                finishAndRemoveTask();
+            }
         }
     }
 
 
+    public void increase(View view) {
+        //increase counter
+        counter ++;
+
+        // check if exist and open file
+        File folder = new File(Environment.getExternalStorageDirectory(),"CounterExample");
+
+        // If folder does not exist
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+
+        // write to the file (timestamp, counter)
+        File file = new File(folder, "counter.csv");
+
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.append(""+System.currentTimeMillis()).append(",").append(""+counter).append("\n");
+            writer.flush();  // flash buffer to the file
+            writer.close(); // closes the stream
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }
 
